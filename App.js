ae847b45bbe655f6e7bb29d8863deafa8caa0a6a
Parth10P <parthkumar200410@gmail.com>
2025-09-30 19:28:57 +0530

    ad bottom navigation testing

--- App.js
+++ App.js
@@ -1,23 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Login from './src/screens/Login';
-import GoogleAuth from './src/screens/GoogleAuth';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Login from "./src/screens/Login";
+import GoogleAuth from "./src/screens/GoogleAuth";
+import { PaperProvider } from "react-native-paper";
+import Home from "./src/screens/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Login/>
-      <GoogleAuth/>
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Home />
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
