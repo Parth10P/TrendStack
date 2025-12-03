e1e382113f6c18c893c2c66119658af944118c11
Parth10P <parthkumar200410@gmail.com>
2025-12-03 13:22:32 +0530

    hosting

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from "expo-status-bar";
+import { StatusBar } from "./node_modules/expo-status-bar/build/StatusBar";
 import { StyleSheet } from "react-native";
 import React, { useState } from "react";
 import { Provider as PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "./node_modules/@react-navigation/native/lib/typescript/src";
+import { createNativeStackNavigator } from "./node_modules/@react-navigation/native-stack/lib/typescript/src";
 import { ThemeProvider } from "./src/context/ThemeContext";
 
 import Login from "./src/screens/Login";
@@ -45,11 +45,7 @@ export default function App() {
                 <>
                   <Stack.Screen name="Home">
                     {(props) => (
-                      <Home
-                        {...props}
-                        user={user}
-                        onLogout={handleLogout}
-                      />
+                      <Home {...props} user={user} onLogout={handleLogout} />
                     )}
                   </Stack.Screen>
                   <Stack.Screen name="Profile">
